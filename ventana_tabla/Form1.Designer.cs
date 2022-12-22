c64307a53549bc019bfb33334713a4669ba17e8a
Conie14 <44936041+Conie14@users.noreply.github.com>
2022-12-22 12:58:26 -0600

    Gnera tabla de multiplicar en windows forms

---
+++ ventana_tabla/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ventana_tabla
+{
+    partial class frmTabla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTabla = new System.Windows.Forms.Label();
+            this.txtTabla = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lstTablita = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTabla
+            // 
+            this.lblTabla.AutoSize = true;
+            this.lblTabla.Location = new System.Drawing.Point(51, 54);
+            this.lblTabla.Name = "lblTabla";
+            this.lblTabla.Size = new System.Drawing.Size(107, 13);
+            this.lblTabla.TabIndex = 0;
+            this.lblTabla.Text = "¿Que tablita deseas?";
+            // 
+            // txtTabla
+            // 
+            this.txtTabla.Location = new System.Drawing.Point(213, 54);
+            this.txtTabla.Name = "txtTabla";
+            this.txtTabla.Size = new System.Drawing.Size(100, 20);
+            this.txtTabla.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(127, 141);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lstTablita
+            // 
+            this.lstTablita.FormattingEnabled = true;
+            this.lstTablita.Location = new System.Drawing.Point(127, 181);
+            this.lstTablita.Name = "lstTablita";
+            this.lstTablita.Size = new System.Drawing.Size(128, 186);
+            this.lstTablita.TabIndex = 3;
+            // 
+            // frmTabla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(437, 378);
+            this.Controls.Add(this.lstTablita);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTabla);
+            this.Controls.Add(this.lblTabla);
+            this.Name = "frmTabla";
+            this.Text = "Tablas de Multiplicar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTabla;
+        private System.Windows.Forms.TextBox txtTabla;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lstTablita;
+    }
+}
+
---
+++ ventana_tabla/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libreria_tabla;
+namespace ventana_tabla
+{
+    public partial class frmTabla : Form
+    {
+        public frmTabla()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Tabla algo = new Tabla();
+            algo.p_numero = Convert.ToInt32(txtTabla.Text);
+            lstTablita.Items.Clear();
+            algo.multiplicar(lstTablita);
+        }
+    }
+}
